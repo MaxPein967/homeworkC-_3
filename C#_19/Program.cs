9c1936325fd500de511441377b91334a0c26f08a
Sergei Vasiliev <9670105747@mail.ru>
2023-02-09 10:21:36 +0700

    Add Palindrom

--- C#_19/Program.cs
+++ C#_19/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 19
+// Напишите программу, которая принимает на вход пятизначное 
+// число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+string str = Console.ReadLine();
+int b;
+if (int.TryParse(str, out b))
+{
+    string rev_str = "";
+    for (int i = str.Length - 1; i >= 0; i--)
+    {
+        rev_str = rev_str + str[i];
+    }
+    int a = Convert.ToInt32(str);
+    b = Convert.ToInt32(rev_str);
+
+    if (a != b)
+    {
+        Console.WriteLine("нет");
+    }
+    else
+        Console.WriteLine("да");
+}
+else
+{
+    Console.WriteLine("Вы должны ввести число...");
+}

cddc4bc20969e421cf31791ccdaf8d624d1971ba
Sergei Vasiliev <9670105747@mail.ru>
2023-02-09 10:24:13 +0700

    Add cube numbers

--- C#_23/Program.cs
+++ C#_23/Program.cs
@@ -1,2 +1,19 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 23
+// Напишите программу, которая принимает на вход число 
+// (N) и выдаёт таблицу кубов чисел от 1 до N.
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number>0)
+{
+    Console.WriteLine("Число \t Куб");
+for (int i = 1; i < number + 1; i++)
+{
+    Console.WriteLine("{0} \t {1}", i, i * i * i);
+}
+}
+else Console.WriteLine("Введите положительное число");
+
